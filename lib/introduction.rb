b8b8abcd259e63e52c87a1b6d3bf46eb144c7967
Shawna Davis <shawna_davis@alum.mit.edu>
2018-07-02 04:11:18 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction2(name, name2)
+def introduction_with_language(name, name2)
   puts "Hi, my name is #{name} and #{name2}."
 end
